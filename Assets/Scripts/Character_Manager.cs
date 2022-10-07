b27bb56cd5316b4e2138aea5bf0fa1952170b7f3
Mohit Shivhare <67676938+Mohit-Cubedots@users.noreply.github.com>
2022-10-07 13:02:26 +0530

    Project Upload

---
+++ Assets/Scripts/Character_Manager.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Character_Manager : MonoBehaviour
+{
+    private Input_Manager _input;
+    public float MoveSpeed = 2.0f;
+
+    private CharacterController _controller;
+    private bool _hasAnimator;
+    private Animator _animator;
+    private float _speed;
+    private float _animationBlend;
+    private float _targetRotation = 0.0f;
+    private GameObject _mainCamera;
+
+    private float _rotationVelocity;
+    private float _verticalVelocity;
+    private int _animIDSpeed;
+    private int _animIDMotionSpeed;
+
+    public float SpeedChangeRate = 10.0f;
+    public float RotationSmoothTime = 0.12f;
+
+    private PlayerInput _playerInput;
+
+
+    private const float _threshold = 0.01f;
+    public bool LockCameraPosition = false;
+
+
+    private float _cinemachineTargetYaw;
+    private float _cinemachineTargetPitch;
+
+
+    public float TopClamp = 70.0f;
+    public float BottomClamp = -30.0f;
+
+    public float CameraAngleOverride = 0.0f;
+
+    public GameObject CinemachineCameraTarget;
+
+    private bool IsCurrentDeviceMouse
+    {
+        get
+        {
+            return _playerInput.currentControlScheme == "KeyboardMouse";
+        }
+    }
+
+    private void Awake()
+    {
+        if (_mainCamera == null)
+        {
+            _mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        }
+    }
+
+    void Start()
+    {
+        _cinemachineTargetYaw = CinemachineCameraTarget.transform.rotation.eulerAngles.y;
+
+        _hasAnimator = TryGetComponent(out _animator);
+        _controller = GetComponent<CharacterController>();
+        _input = GetComponent<Input_Manager>();
+        _playerInput = GetComponent<PlayerInput>();
+        AssignAnimationIDs();
+    }
+
+    void Update()
+    {
+        _hasAnimator = TryGetComponent(out _animator);
+
+        Move();
+    }
+
+    private void LateUpdate()
+    {
+        CameraRotation();
+    }
+
+    private void AssignAnimationIDs()
+    {
+        _animIDSpeed = Animator.StringToHash("Speed");
+        _animIDMotionSpeed = Animator.StringToHash("MotionSpeed");
+    }
+
+    private void Move()
+    {
+        float targetSpeed =  MoveSpeed;
+
+        if (_input.move == Vector2.zero)
+            targetSpeed = 0.0f;
+
+        float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
+
+        float speedOffset = 0.1f;
+        float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
+
+        if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
+        {
+            _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude,
+                Time.deltaTime * SpeedChangeRate);
+
+            _speed = Mathf.Round(_speed * 1000f) / 1000f;
+        }
+        else
+        {
+            _speed = targetSpeed;
+        }
+
+        _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * SpeedChangeRate);
+
+        if (_animationBlend < 0.01f)
+            _animationBlend = 0f;
+
+        Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
+
+        if (_input.move != Vector2.zero)
+        {
+            _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + _mainCamera.transform.eulerAngles.y;
+            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,RotationSmoothTime);
+
+            transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+        }
+
+
+        Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+
+        _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+
+        if (_hasAnimator)
+        {
+            _animator.SetFloat(_animIDSpeed, _animationBlend);
+            _animator.SetFloat(_animIDMotionSpeed, inputMagnitude);
+        }
+    }
+    private void CameraRotation()
+    {
+        if (_input.look.sqrMagnitude >= _threshold && !LockCameraPosition)
+        {
+            float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
+
+            _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier;
+            _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier;
+        }
+
+        _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
+        _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, BottomClamp, TopClamp);
+
+        CinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch + CameraAngleOverride, _cinemachineTargetYaw, 0.0f);
+    }
+
+    private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+    {
+        if (lfAngle < -360f) lfAngle += 360f;
+        if (lfAngle > 360f) lfAngle -= 360f;
+        return Mathf.Clamp(lfAngle, lfMin, lfMax);
+    }
+}
---
+++ Assets/Scripts/Input_Manager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Input_Manager : MonoBehaviour
+{
+    public Vector2 move;
+    public Vector2 look;
+
+    public bool analogMovement;
+    public bool cursorInputForLook = true;
+
+    public void OnMove(InputValue value)
+    {
+        MoveInput(value.Get<Vector2>());
+    }
+
+    public void OnLook(InputValue value)
+    {
+        if (cursorInputForLook)
+        {
+            LookInput(value.Get<Vector2>());
+        }
+    }
+
+    public void MoveInput(Vector2 newMoveDirection)
+    {
+        move = newMoveDirection;
+    }
+
+    public void LookInput(Vector2 newLookDirection)
+    {
+        look = newLookDirection;
+    }
+}
